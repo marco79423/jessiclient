9678205a7a83c27f02b993950f1c465f2c393328
Marco Li <marco_li@paradise-soft.com.tw>
2021-02-14 19:40:27 +0800

    調整 logo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>Jessiclient</title>
+
+        <link rel="icon" href="/favicon.ico"/>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+      </Head>
+
+      <CssBaseline/>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
