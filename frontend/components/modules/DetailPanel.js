a3e0d41bd6ae8a40d71064f5b07a1a475f957c2e
marco79423 <marco79423@gmail.com>
2021-02-28 21:34:14 +0800

    新增 JSON 的展示方式

--- frontend/components/modules/DetailPanel.js
+++ frontend/components/modules/DetailPanel.js
@@ -1,47 +1,87 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import classNames from 'classnames'
 import {makeStyles} from '@material-ui/core/styles'
-import {Paper, Tab, Tabs, TextField} from '@material-ui/core'
+import {Paper, Tab, Tabs, TextField, Toolbar} from '@material-ui/core'
 
-import {getHistory, getSelectedHistoryID} from '../../slices'
+import {getHistory} from '../../slices'
+import ReactJson from 'react-json-view'
+import {TabContext, TabPanel} from '@material-ui/lab'
 
 
 const useStyles = makeStyles((theme) => ({
   root: {},
+  controlBar: {
+    height: 64,
+  },
+  dataSection: {
+    padding: 0,
+    position: 'relative',
+    overflow: 'auto',
+    height: 'calc(100vh - 64px)',
+  }
 }))
 
 export default function RequestPanel({className}) {
   const classes = useStyles()
+  const [tabValue, setTabValue] = useState('plain-text')
+  const [jsonData, setJsonData] = useState(null)
 
-  const historyID = useSelector(getSelectedHistoryID)
   const history = useSelector(getHistory)
 
-  if (historyID === null) {
+  useEffect(() => {
+    try {
+      setJsonData(JSON.parse(history.text))
+      setTabValue('json')
+    } catch (_) {
+      setJsonData(null)
+      setTabValue('plain-text')
+    }
+  }, [history])
+
+
+  if (!history) {
     return (
       <div className={classNames(classes.root, className)}>
       </div>
     )
   }
 
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue)
+  }
+
   return (
     <div className={classNames(classes.root, className)}>
-      <Tabs indicatorColor="primary" style={{marginTop: 48}} value={0}>
-        <Tab label="基本"/>
-      </Tabs>
-      <Paper style={{padding: 16}}>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          multiline
-          rows={24}
-          fullWidth
-          autoFocus
-          value={history.text}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
+      <Toolbar className={classes.controlBar}>
+        <Tabs value={tabValue} onChange={handleTabChange}>
+          <Tab label="純文字" value="plain-text"/>
+          <Tab label="JSON" value="json" disabled={jsonData === null}/>
+        </Tabs>
+      </Toolbar>
+      <Paper className={classes.dataSection}>
+        <TabContext value={tabValue}>
+          <TabPanel value="plain-text">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              multiline
+              rows={24}
+              fullWidth
+              autoFocus
+              value={history.text}
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          </TabPanel>
+          <TabPanel value="json">
+            <ReactJson
+              src={jsonData}
+              indentWidth={2}
+            />
+          </TabPanel>
+        </TabContext>
       </Paper>
     </div>
   )
