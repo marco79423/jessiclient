bddd03c8bbf7f3c37570b4391726dbdfc3ef10c3
marco79423 <marco79423@gmail.com>
2021-03-13 10:52:40 +0800

    移除 AppBar，新增 Toolbar

--- frontend/components/modules/Toolbar.js
+++ frontend/components/modules/Toolbar.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {makeStyles} from '@material-ui/core/styles'
-import {AppBar as MuiAppBar, Avatar, Grid, IconButton, TextField, Toolbar, Tooltip, Typography} from '@material-ui/core'
+import {Grid, IconButton, TextField, Toolbar as MuiToolbar, Tooltip} from '@material-ui/core'
 import ArchiveIcon from '@material-ui/icons/Archive'
 import UnarchiveIcon from '@material-ui/icons/Unarchive'
 import SettingsIcon from '@material-ui/icons/Settings'
@@ -13,23 +13,6 @@ import Button from '../elements/Button'
 
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    background: theme.project.page.header.background,
-  },
-  brand: {
-    width: 180,
-  },
-  logo: {
-    margin: '3px auto 0',
-    width: 45,
-    height: 45,
-  },
-  title: {
-    color: theme.project.page.header.titleColor,
-  },
-  subtitle: {
-    color: theme.project.page.header.subtitleColor,
-  },
   settingsButton: {
     color: theme.project.page.header.settingsButton,
   },
@@ -38,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function AppBar() {
+export default function Toolbar() {
   const classes = useStyles()
   const dispatch = useDispatch()
   const [settingsPanelOpen, setSettingsPanel] = useState(false)
@@ -66,50 +49,37 @@ export default function AppBar() {
 
   return (
     <>
-      <MuiAppBar className={classes.root} position="relative" elevation={1}>
-        <Toolbar>
-          <Grid container justify="space-between">
-            <Grid className={classes.brand} container item alignItems="center">
-              <Grid xs={4} item>
-                <Avatar className={classes.logo} variant="square" src={'/favicon.ico'}/>
-              </Grid>
-              <Grid xs={8} item container>
-                <Typography className={classes.title} component="h1" variant="h5">Jessiclient</Typography>
-                <Typography className={classes.subtitle} component="h2" variant="subtitle2">Websocket
-                  Client</Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Tooltip title="分享">
-                <IconButton style={{visibility: 'hidden'}} className={classes.settingsButton}
-                            onClick={showSettingsPanel}>
-                  <ShareIcon className={classes.icon}/>
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="匯出專案">
-                <IconButton className={classes.settingsButton} onClick={showExportPanel}>
-                  <ArchiveIcon className={classes.icon}/>
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="匯入專案">
-                <IconButton className={classes.settingsButton} onClick={onImportClicked}>
-                  <UnarchiveIcon className={classes.icon}/>
-                </IconButton>
-              </Tooltip>
-              <IconButton className={classes.settingsButton} onClick={showSettingsPanel}>
-                <SettingsIcon className={classes.icon}/>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <MuiToolbar>
+            <Tooltip title="分享">
+              <IconButton style={{visibility: 'hidden'}} className={classes.settingsButton}
+                          onClick={showSettingsPanel}>
+                <ShareIcon className={classes.icon}/>
               </IconButton>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </MuiAppBar>
+            </Tooltip>
+            <Tooltip title="匯出專案">
+              <IconButton className={classes.settingsButton} onClick={showExportPanel}>
+                <ArchiveIcon className={classes.icon}/>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="匯入專案">
+              <IconButton className={classes.settingsButton} onClick={onImportClicked}>
+                <UnarchiveIcon className={classes.icon}/>
+              </IconButton>
+            </Tooltip>
+            <IconButton className={classes.settingsButton} onClick={showSettingsPanel}>
+              <SettingsIcon className={classes.icon}/>
+            </IconButton>
+          </MuiToolbar>
+        </Grid>
+      </Grid>
 
       <ExportPanel open={exportPanelOpen} onClose={hideExportPanel}/>
       <SettingsPanel open={settingsPanelOpen} onClose={hideSettingsPanel}/>
     </>
   )
 }
-
 
 function ExportPanel({open, onClose}) {
   const dispatch = useDispatch()
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,13 +2,13 @@ import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import {makeStyles} from '@material-ui/core/styles'
-import {Grid, Slide} from '@material-ui/core'
+import {AppBar as MuiAppBar, Avatar, Grid, Slide, Typography} from '@material-ui/core'
 
 import {getSelectedMessageID, initialize} from '../slices'
 import ListPanel from '../components/modules/ListPanel'
 import ControlPanel from '../components/modules/ControlPanel'
 import DetailPanel from '../components/modules/DetailPanel'
-import AppBar from '../components/modules/AppBar'
+import Toolbar from '../components/modules/Toolbar'
 
 export const getStaticProps = async ({locale}) => ({
   props: {
@@ -18,6 +18,23 @@ export const getStaticProps = async ({locale}) => ({
 
 const useStyles = makeStyles((theme) => ({
   root: {},
+  abbBar: {
+    background: theme.project.page.header.background,
+  },
+  brand: {
+    width: 180,
+  },
+  logo: {
+    margin: '3px auto 0',
+    width: 45,
+    height: 45,
+  },
+  title: {
+    color: theme.project.page.header.titleColor,
+  },
+  subtitle: {
+    color: theme.project.page.header.subtitleColor,
+  },
   main: {
     background: theme.project.page.main.background,
     height: 'calc(100vh - 64px)'
@@ -42,7 +59,16 @@ export default function Index() {
 
   return (
     <>
-      <AppBar/>
+      <MuiAppBar className={classes.abbBar} position="relative" elevation={1}>
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item>
+            <Logo/>
+          </Grid>
+          <Grid item>
+            <Toolbar/>
+          </Grid>
+        </Grid>
+      </MuiAppBar>
       <Grid className={classes.main} container component="main">
         <Grid className={classes.controlPanel} item sm={4} xs={12}>
           <ControlPanel/>
@@ -57,5 +83,21 @@ export default function Index() {
         </Slide>
       </Grid>
     </>
+  )
+}
+
+function Logo() {
+  const classes = useStyles()
+
+  return (
+    <Grid className={classes.brand} container item alignItems="center">
+      <Grid xs={4} item>
+        <Avatar className={classes.logo} variant="square" src={'/favicon.ico'}/>
+      </Grid>
+      <Grid xs={8} item container>
+        <Typography className={classes.title} component="h1" variant="h5">Jessiclient</Typography>
+        <Typography className={classes.subtitle} component="h2" variant="subtitle2">Websocket Client</Typography>
+      </Grid>
+    </Grid>
   )
 }
