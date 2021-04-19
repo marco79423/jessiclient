f084e3b8a950ff915d32d02734115d1c92018b29
Marco Li <marco_li@paradise-soft.com.tw>
2021-04-19 10:12:48 +0800

    Button 新增 memo

--- frontend/components/elements/Button.js
+++ frontend/components/elements/Button.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {memo} from 'react'
 import PropTypes from 'prop-types'
 import {Button as MuiButton} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
@@ -9,7 +9,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function Button({children, className, primary, disabled, onClick}) {
+function Button({children, className, primary, disabled, onClick}) {
   const classes = useStyles()
 
   return (
@@ -17,7 +17,7 @@ export default function Button({children, className, primary, disabled, onClick}
       className={`${classes.root} ${className}`}
       color={primary ? 'primary' : 'default'}
       disabled={disabled}
-      variant='contained'
+      variant="contained"
       onClick={onClick}>
       {children}
     </MuiButton>
@@ -31,3 +31,5 @@ Button.propTypes = {
   disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 }
+
+export default memo(Button)
