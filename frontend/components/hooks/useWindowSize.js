dfdcd3d35f51bb7e46763713aee94e4d927b41a1
Marco Li <marco_li@paradise-soft.com.tw>
2021-04-29 10:02:57 +0800

    useWindowSize 改用 useEffect

--- frontend/components/hooks/useWindowSize.js
+++ frontend/components/hooks/useWindowSize.js
@@ -1,14 +1,15 @@
-import {useLayoutEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 
 export default function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
+  const [size, setSize] = useState([0, 0])
+  useEffect(() => {
     function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
+      setSize([window.innerWidth, window.innerHeight])
     }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
+
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+  return size
 }
