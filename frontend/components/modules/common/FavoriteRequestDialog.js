aa8579acf7b37feee6f40855291c2f8a9ec9b611
marco79423 <marco79423@gmail.com>
2021-07-25 14:21:38 +0800

    常用列表新增篩選分類

--- frontend/components/modules/common/FavoriteRequestDialog.js
+++ frontend/components/modules/common/FavoriteRequestDialog.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {useTranslation} from 'next-i18next'
-import {Grid, Paper} from '@material-ui/core'
+import lodash from 'lodash'
+import {Grid} from '@material-ui/core'
 import BasicDialog from '../../elements/BasicDialog'
-import Button from '../../elements/Button'
-import EditableText from '../../elements/EditableText'
 import {makeStyles} from '@material-ui/core/styles'
+import Select from '../../elements/Select'
+import FavoriteRequestItem from './FavoriteRequestItem'
 
+
+const useStyles = makeStyles((theme) => ({
+  filter: {
+    marginBottom: theme.spacing(2)
+  }
+}))
 
 export default function FavoriteRequestDialog({
                                                 isConnected,
@@ -14,15 +21,41 @@ export default function FavoriteRequestDialog({
                                                 favoriteRequests,
                                                 onRemove, onApply, onSend, onUpdate
                                               }) {
-
+  const classes = useStyles()
   const {t} = useTranslation('ControlPanel')
+  const [currentFilterSelectionValue, setCurrentFilterSelectionValue] = useState()
 
-  const favoriteRequestCategories = favoriteRequests.map(favoriteRequest => favoriteRequest.category)
+  const favoriteRequestCategories = React.useMemo(() => lodash(favoriteRequests)
+    .map(favoriteRequest => favoriteRequest.category)
+    .uniq()
+    .value(), [favoriteRequests])
+
+  const filteredSelections = React.useMemo(() => favoriteRequestCategories
+    .map(category => ({
+      key: category,
+      label: category,
+      value: category,
+    })), [favoriteRequestCategories])
+
+  const filteredFavoriteRequests = React.useMemo(() => favoriteRequests
+    .filter(favoriteRequest => favoriteRequest.category === currentFilterSelectionValue), [favoriteRequests, currentFilterSelectionValue])
+
+  useEffect(() => {
+    if (favoriteRequestCategories.length > 0 && !favoriteRequestCategories.includes(currentFilterSelectionValue)) {
+      setCurrentFilterSelectionValue(favoriteRequestCategories[0])
+    }
+  }, [favoriteRequestCategories])
 
   return (
     <BasicDialog title={t('常用請求列表')} autoFullScreen open={open} onClose={onClose}>
+      <Select
+        className={classes.filter}
+        currentValue={currentFilterSelectionValue}
+        selections={filteredSelections}
+        onSelectionChange={setCurrentFilterSelectionValue}
+      />
       <Grid container spacing={2} justify="center">
-        {favoriteRequests.map(favoriteRequest => (
+        {filteredFavoriteRequests.map(favoriteRequest => (
           <FavoriteRequestItem
             key={favoriteRequest.id}
             isConnected={isConnected}
@@ -56,106 +89,3 @@ FavoriteRequestDialog.propTypes = {
   onUpdate: PropTypes.func.isRequired,
 }
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-    width: 350,
-    minHeight: 200,
-  },
-  title: {
-    fontSize: '1.2rem',
-  },
-  category: {
-    fontSize: '1.1rem',
-  },
-  content: {
-    marginTop: theme.spacing(2),
-  }
-}))
-
-
-function FavoriteRequestItem({isConnected, favoriteRequest, onRemove, onApply, onSend, onUpdate}) {
-  const classes = useStyles()
-  const {t} = useTranslation('ControlPanel')
-
-  const updateName = (name) => {
-    onUpdate({id: favoriteRequest.id, changes: {name}})
-  }
-
-  const updateCategory = (category) => {
-    onUpdate({id: favoriteRequest.id, changes: {category}})
-  }
-
-  const updateBody = (body) => {
-    onUpdate({id: favoriteRequest.id, changes: {body}})
-  }
-
-  const onApplyButtonClicked = () => {
-    onApply(favoriteRequest)
-  }
-
-  const onRemoveButtonClicked = () => {
-    onRemove(favoriteRequest.id)
-  }
-
-  const onSendButtonClicked = () => {
-    onSend(favoriteRequest)
-  }
-
-  return (
-    <Grid key={favoriteRequest.id} item>
-      <Grid className={classes.root} container component={Paper} direction="column" justify="space-between">
-        <Grid item>
-          <Grid container justify="space-between">
-            <EditableText
-              className={classes.title}
-              value={favoriteRequest.name}
-              setValue={updateName}
-              buttonLabel={t('儲存')}
-            />
-            <EditableText
-              className={classes.category}
-              placeholder={'分類'}
-              value={favoriteRequest.category}
-              setValue={updateCategory}
-              buttonLabel={t('儲存')}
-            />
-          </Grid>
-          <EditableText
-            className={classes.content}
-            value={favoriteRequest.body}
-            setValue={updateBody}
-            buttonLabel={t('儲存')}
-          />
-        </Grid>
-        <Grid container item>
-          <Grid container item xs spacing={1}>
-            <Grid item><Button primary onClick={onApplyButtonClicked}>{t('套用')}</Button></Grid>
-            <Grid item><Button onClick={onRemoveButtonClicked}>{t('刪除')}</Button> </Grid>
-          </Grid>
-          <Grid item>
-            <Grid item>
-              <Button primary disabled={!isConnected} onClick={onSendButtonClicked}>{t('直接送出')}</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
-  )
-}
-
-FavoriteRequestItem.propTypes = {
-  isConnected: PropTypes.bool.isRequired,
-
-  favoriteRequest: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-  }).isRequired,
-
-  onRemove: PropTypes.func.isRequired,
-  onApply: PropTypes.func.isRequired,
-  onSend: PropTypes.func.isRequired,
-  onUpdate: PropTypes.func.isRequired,
-}
